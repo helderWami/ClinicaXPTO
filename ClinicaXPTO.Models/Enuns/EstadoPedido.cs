1b5deccb43b31963fb243a0302e3a95a56b4dfbc
helton-evambi <heltonevambi@gmail.com>
2025-06-27 22:06:58 +0100

    feat: add descriptions to EstadoPedido enum values

--- ClinicaXPTO.Models/Enuns/EstadoPedido.cs
+++ ClinicaXPTO.Models/Enuns/EstadoPedido.cs
@@ -1,9 +1,16 @@
-﻿namespace ClinicaXPTO.Models.Enuns
+﻿using System.ComponentModel;
+
+namespace ClinicaXPTO.Models.Enuns
 {
     public enum EstadoPedido
     {
+        [Description("Pendente")]
         Pendente = 0,
+
+        [Description("Agendado")]
         Agendado = 1,
+
+        [Description("Realizado")]
         Realizado = 2
     }
 }
