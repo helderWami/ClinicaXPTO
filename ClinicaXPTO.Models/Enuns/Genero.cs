5b4a140478da96121d3e45ffef05f4a11dfde4f8
helton-evambi <heltonevambi@gmail.com>
2025-06-27 22:07:33 +0100

    feat: add descriptions to Genero enum values

--- ClinicaXPTO.Models/Enuns/Genero.cs
+++ ClinicaXPTO.Models/Enuns/Genero.cs
@@ -1,8 +1,13 @@
-﻿namespace ClinicaXPTO.Models.Enuns
+﻿using System.ComponentModel;
+
+namespace ClinicaXPTO.Models.Enuns
 {
     public enum Genero
     {
+        [Description("Masculino")]
         Masculino = 0,
+
+        [Description("Feminino")]
         Feminino = 1
     }
 }
