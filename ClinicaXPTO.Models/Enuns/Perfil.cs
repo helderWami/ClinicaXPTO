2db43b0d4d7a11764a0b27f0237ce9a0dba1ddd6
helton-evambi <heltonevambi@gmail.com>
2025-06-27 22:07:48 +0100

    feat: add descriptions to Perfil enum values

--- ClinicaXPTO.Models/Enuns/Perfil.cs
+++ ClinicaXPTO.Models/Enuns/Perfil.cs
@@ -1,9 +1,16 @@
-﻿namespace ClinicaXPTO.Models.Enuns
+﻿using System.ComponentModel;
+
+namespace ClinicaXPTO.Models.Enuns
 {
     public enum Perfil
     {
+        [Description("Utente Registado")]
         UtenteRegistado = 0,
+
+        [Description("Administrativo")]
         Administrativo = 1,
+
+        [Description("Administrador da Aplicação")]
         Administrador = 2
     }
 }
