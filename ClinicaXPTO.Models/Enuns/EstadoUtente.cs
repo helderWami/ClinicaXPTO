315689019364da18bcca11b2b2ae96e7db68fc77
helton-evambi <heltonevambi@gmail.com>
2025-06-28 20:56:18 +0100

    feat: add EstadoUtente enum for user registration status

---
+++ ClinicaXPTO.Models/Enuns/EstadoUtente.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace ClinicaXPTO.Models.Enuns
+{
+    public enum EstadoUtente
+    {
+        [Description("Utente Registado")]
+        UtenteRegistado = 0,
+
+        [Description("Utente Anónimo")]
+        UtenteAnonimo = 1, // Anónimo, não registado
+    }
+}
